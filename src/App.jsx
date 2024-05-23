f83b5423e39c1b75d2ab5ad5ada1effc7e212f89
YONGHEE <ayh0429@gmail.com>
2024-05-24 00:54:03 +0900

    add searching filter and test mock data

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,21 @@ import { useReducer, useRef, createContext } from "react";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-05-23").getTime(),
     emotionId: 1,
     content: "1번 일기",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-05-22").getTime(),
     emotionId: 2,
     content: "2번 일기",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-04-29").getTime(),
+    emotionId: 3,
+    content: "3번 일기",
   },
 ];
 
@@ -36,8 +42,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
